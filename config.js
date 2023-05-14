c138287b8356d6c460e4bcdc61988cb2979a9045
clemensgg <csc@soundportal.at>
2023-05-14 17:10:25 +0200

    doesn't need prettyname

--- config.js
+++ config.js
@@ -1,15 +1,13 @@
 const config = {
-    "port": 3000,
-    "chains": [
-      {
-        "prettyName": "Cosmos Hub",
-        "rpcUrl": "https://cosmoshub-rpc.lavenderfive.com:443"
-      },
-      {
-        "prettyName": "Osmosis",
-        "rpcUrl": "https://osmosis-rpc.lavenderfive.com:443"
-      }
-    ]
-  };
-  
-  module.exports = config;
+  "port": 3000,
+  "chains": [
+    {
+      "rpcUrl": "https://cosmoshub-rpc.lavenderfive.com:443"
+    },
+    {
+      "rpcUrl": "https://osmosis-rpc.polkachu.com"
+    }
+  ]
+};
+
+export default config;
